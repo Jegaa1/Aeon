db52023d5d5bbbee54b81b10e4620d31987313da
Jegaa1 <121353390+Jegaa1@users.noreply.github.com>
2023-12-18 03:11:03 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -74,7 +74,7 @@ def get_readable_file_size(size_in_bytes):
         return '0B'
     index = 0
     while size_in_bytes >= 1024 and index < len(SIZE_UNITS) - 1:
-        size_in_bytes /= 2048
+        size_in_bytes /= 512
         index += 1
     return f'{size_in_bytes:.2f}{SIZE_UNITS[index]}' if index > 0 else f'{size_in_bytes}B'
 
